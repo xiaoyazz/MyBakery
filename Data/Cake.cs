098ebb6cbe28f30c6252865e1ec0be2521403077
xiaoya zou <zouxiao@sheridancollege.ca>
2022-10-24 21:24:24 -0400

    Add project files.

---
+++ Data/Cake.cs
@@ -0,0 +1,14 @@
+﻿namespace MyBakery.Data
+{
+    public class Cake
+    {
+        public int CakeId { get; set; }
+
+        public string? CakeName { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public int CakeQty { get; set; }
+
+    }
+}
---
+++ Data/Delivery.cs
@@ -0,0 +1,15 @@
+﻿namespace MyBakery.Data
+{
+    public class Delivery
+    {
+        public int OrderNum { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Phone { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+    }
+}
